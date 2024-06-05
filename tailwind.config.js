cf228866d764752e853e5935be72a107d6bbb3a6
Léo Audibert <leonard.audibert@gmail.com>
2024-06-05 12:01:40 -0700

    Update styles [skip ci]

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,88 +11,32 @@ module.exports = {
         lightmode: {
           // Change to any existing daisyui theme or make your own
           ...require("daisyui/src/theming/themes")["cmyk"],
-
-          // // Edit/override styles if required
-          // primary: "#FFD500",
-          // "primary-focus": "#FFDC2C",
-          // "primary-content": "#1b1c22",
-
-          // secondary: "#9c00f0",
-          // "secondary-focus": "#b429ff",
-          // "secondary-content": "#ffffff",
-
-          // accent: "#37cdbe",
-          // "accent-focus": "#2ba69a",
-          // "accent-content": "#ffffff",
-
-          // neutral: "#3b424e",
-          // "neutral-focus": "#2a2e37",
-          // "neutral-content": "#ffffff",
-
-          // "base-100": "#ffffff",
-          // "base-200": "#f9fafb",
-          // "base-300": "#ced3d9",
-          // "base-content": "#1e2734",
-
-          // info: "#1c92f2",
-          // success: "#009485",
-          // warning: "#ff9900",
-          // error: "#ff5724",
-
-          // "--rounded-box": "1rem",
-          // "--rounded-btn": ".5rem",
-          // "--rounded-badge": "1.9rem",
-
-          // "--animation-btn": ".25s",
-          // "--animation-input": ".2s",
-
-          // "--btn-text-case": "uppercase",
-          // "--navbar-padding": ".5rem",
-          // "--border-btn": "1px",
+          // Edit styles if required
+          primary: "white",
+          secondary: "#DEFBFB",
+          accent: "#FF5D73",
+          neutral: "#919191",
+          // "base-100": "",
+          info: "#623CEA",
+          success: "#87FF65",
+          warning: "#FFC759",
+          error: "#A30000",
         },
       },
       {
         darkmode: {
           // Change to any existing daisyui theme or make your own
-          ...require("daisyui/src/theming/themes")["sunset"],
-
-          // // Edit/override styles if required
-          // primary: "#FFD500",
-          // "primary-focus": "#FFDC2C",
-          // "primary-content": "#1b1c22",
-
-          // secondary: "#b9ffb3",
-          // "secondary-focus": "#8aff80",
-          // "secondary-content": "#1b1c22",
-
-          // accent: "#ffffb3",
-          // "accent-focus": "#ffff80",
-          // "accent-content": "#1b1c22",
-
-          // neutral: "#22212c",
-          // "neutral-focus": "#1b1c22",
-          // "neutral-content": "#d5ccff",
-
-          // "base-100": "#302f3d",
-          // "base-200": "#22212c",
-          // "base-300": "#1b1c22",
-          // "base-content": "#d5ccff",
-
-          // info: "#1c92f2",
-          // success: "#009485",
-          // warning: "#ff9900",
-          // error: "#ff5724",
-
-          // "--rounded-box": "1rem",
-          // "--rounded-btn": ".5rem",
-          // "--rounded-badge": "1.9rem",
-
-          // "--animation-btn": ".25s",
-          // "--animation-input": ".2s",
-
-          // "--btn-text-case": "uppercase",
-          // "--navbar-padding": ".5rem",
-          // "--border-btn": "1px",
+          ...require("daisyui/src/theming/themes")["business"],
+          // Edit styles if required
+          primary: "black",
+          secondary: "#1D1D1D",
+          accent: "blue",
+          neutral: "#494949",
+          "base-100": "#031622",
+          info: "#623CEA",
+          success: "#80D569",
+          warning: "#FFC759",
+          error: "#A30000",
         },
       },
     ],
