367bfc09b7d13cb09cbe1bdd2442b0d2d790e34c
Léo Audibert <leonard.audibert@gmail.com>
2024-06-14 06:49:12 -0700

    Update config test

--- config/config_test.go
+++ config/config_test.go
@@ -11,10 +11,7 @@ func TestGetConfig(t *testing.T) {
 	_, err := GetConfig()
 	require.NoError(t, err)
 
-	var env environment
-	env = "abc"
-	SwitchEnvironment(env)
 	cfg, err := GetConfig()
 	require.NoError(t, err)
-	assert.Equal(t, env, cfg.App.Environment)
+	assert.Equal(t, EnvLocal, cfg.App.Environment)
 }
